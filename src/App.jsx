dc070fb35df16a6dafe93f30a3621226f0bd85a4
cengErhan <erhanjava@gmail.com>
2022-08-05 23:53:44 +0300

    my-first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='bg-primary-base w-1265'>App</h1>
+    </div>
+  )
+}
+
+export default App
